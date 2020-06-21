4cdc3bbd44ca5289210f38b333e44254aee1e58d
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-06-21 09:24:57 +0200

    Rebase from 'debian/noetic/mrpt2'

--- libs/apps/src/RawlogGrabberApp_unittest.cpp
+++ libs/apps/src/RawlogGrabberApp_unittest.cpp
@@ -10,6 +10,7 @@
 #include <gtest/gtest.h>
 #include <mrpt/apps/RawlogGrabberApp.h>
 #include <mrpt/config.h>
+#include <mrpt/core/lock_helper.h>
 #include <mrpt/system/filesystem.h>
 #include <test_mrpt_common.h>
 #include <thread>
@@ -61,13 +62,18 @@ TEST(RawlogGrabberApp, DISABLED_CGenericCamera_AVI)
 		app.show_sensor_thread_exceptions = false;
 
 		const std::size_t REQUIRED_GRAB_OBS = 3U;
+		std::atomic_bool runEnded = false;
 
 		auto tWatchDog = std::thread([&]() {
-			for (;;)
+			while (!runEnded)
 			{
-				if (!app.isRunning()) break;
-				if (app.rawlog_saved_objects > REQUIRED_GRAB_OBS)
+				const auto numSavedObjs = [&]() {
+					auto lk = mrpt::lockHelper(app.results_mtx);
+					return app.rawlog_saved_objects;
+				}();
+				if (numSavedObjs >= REQUIRED_GRAB_OBS)
 				{
+					auto lk = mrpt::lockHelper(app.params_mtx);
 					app.run_for_seconds = 1.0;  // make it exit
 					break;
 				}
@@ -77,6 +83,8 @@ TEST(RawlogGrabberApp, DISABLED_CGenericCamera_AVI)
 
 		// Run:
 		app.run();
+
+		runEnded = true;
 
 		// Check expected results:
 		std::cout << "Rawlog grabbed objects: " << app.rawlog_saved_objects
