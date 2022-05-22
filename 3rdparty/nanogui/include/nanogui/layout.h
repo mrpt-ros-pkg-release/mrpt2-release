bf5bc2e8e80f85e22705038f1f2b5fe9a591beca
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-05-22 07:28:15 +0200

    Rebase from 'release/melodic/mrpt2'

--- 3rdparty/nanogui/include/nanogui/layout.h
+++ 3rdparty/nanogui/include/nanogui/layout.h
@@ -419,8 +419,8 @@ public:
 
         /// Allows for printing out Anchor position, size, and alignment.
         operator std::string() const {
-            char buf[50];
-            std::snprintf(buf, 50, "Format[pos=(%i, %i), size=(%i, %i), align=(%i, %i)]",
+            char buf[100];
+            std::snprintf(buf, 100, "Format[pos=(%i, %i), size=(%i, %i), align=(%i, %i)]",
                 pos[0], pos[1], size[0], size[1], (int) align[0], (int) align[1]);
             return buf;
         }
