9f49cd4dba67c9b6dae037910ee1f9ddabdd384e
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-11-25 21:42:04 +0100

    Rebase from 'debian/noetic/mrpt2'

--- 3rdparty/nanoflann/examples/pointcloud_custom_metric.cpp
+++ 3rdparty/nanoflann/examples/pointcloud_custom_metric.cpp
@@ -109,23 +109,23 @@ static void kdtree_custom_metric_demo(const size_t N)
         num_t                          out_dist_sqr;
         nanoflann::KNNResultSet<num_t> resultSet(num_results);
         resultSet.init(&ret_index, &out_dist_sqr);
-        index.findNeighbors(
-            resultSet, &query_pt[0], nanoflann::SearchParams(10));
+        index.findNeighbors(resultSet, &query_pt[0]);
 
-        std::cout << "knnSearch(nn=" << num_results << "): \n";
+        std::cout << "knnSearch(nn=" << num_results << "\n";
         std::cout << "ret_index=" << ret_index
                   << " out_dist_sqr=" << out_dist_sqr << endl;
     }
     {
         // Unsorted radius search:
-        const num_t                           radius = 1;
-        std::vector<std::pair<size_t, num_t>> indices_dists;
-        RadiusResultSet<num_t, size_t>        resultSet(radius, indices_dists);
+        const num_t                                       radius = 1;
+        std::vector<nanoflann::ResultItem<size_t, num_t>> indices_dists;
+        RadiusResultSet<num_t, size_t> resultSet(radius, indices_dists);
 
-        index.findNeighbors(resultSet, query_pt, nanoflann::SearchParams());
+        index.findNeighbors(resultSet, query_pt);
 
         // Get worst (furthest) point, without sorting:
-        std::pair<size_t, num_t> worst_pair = resultSet.worst_item();
+        nanoflann::ResultItem<size_t, num_t> worst_pair =
+            resultSet.worst_item();
         cout << "Worst pair: idx=" << worst_pair.first
              << " dist=" << worst_pair.second << endl;
     }
